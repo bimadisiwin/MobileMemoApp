56a78a70bec987d7078321f4435984b312c81874
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2021-04-28 18:49:25 +0900

    インタラクティブな動作を追加

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,23 @@
-import { string } from 'prop-types';
+import { func, string } from 'prop-types';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-export default function Buttom(props) {
-  const { label } = props;
+export default function Button(props) {
+  const { label, onPress } = props;
   return (
-    <View style={styles.buttonContainer}>
+    <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
       <Text style={styles.buttonLabel}>{label}</Text>
-    </View>
+    </TouchableOpacity>
   );
 }
 
-Buttom.propTypes = {
+Button.propTypes = {
   label: string.isRequired,
+  onPress: func,
+};
+
+Button.defaultProps = {
+  onPress: null,
 };
 
 const styles = StyleSheet.create({
